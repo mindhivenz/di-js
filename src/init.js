6996f13fcfa7b42e5e844400049f8d54e7b60c3b
Damon <damon@mahuika.co.nz>
2016-09-11 22:04:47 +0900

    Doc changes

--- src/init.js
+++ src/init.js
@@ -1,8 +1,7 @@
 import { appContext } from './inject'
 
 /*
- A module should be defined as the index.js in a directory with a default export of
- a function (appContext).
+ A module should be defined as default export as a function the takes (appContext).
 
  A module can add to the appContext by returning an object from this function where
  the properties are the names and objects to add to the appContext. For example:
@@ -10,8 +9,7 @@ import { appContext } from './inject'
  export default ({ existingService }) => { newService: new Service(existingService) }
 
  The order is important as each module adds new services into the context, and then
- the context is passed into the next module. Also, the module functions will be called
- honoring Meteor.startup().
+ the context is passed into the next module.
  */
 
 const sanityCheckDuplicateContextName = (newNames) => {
