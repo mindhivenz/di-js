53b9fd22b188ab9e8ee73b42fbd540e7f52787ba
Damon <damon@mahuika.co.nz>
2017-09-26 14:39:24 +0200

    Remove reliance on global package as it's bringing in too much

--- src/appContext.js
+++ src/appContext.js
@@ -1,16 +1,27 @@
-import global from 'global'
-
 /*
  Contains all the services, etc. that need to be injected (i.e. Dependency Injection)
 
  Only exported to enable testing. You shouldn't be accessing this normally.
  */
 
-if (typeof global.appContext === 'undefined') {
-  global.appContext = {}
+const chooseGlobalNamespace = () => {
+  if (typeof window !== 'undefined') {
+    return window
+  } else if (typeof global !== 'undefined') {
+    return global
+  } else if (typeof self !== 'undefined') {
+    return self
+  }
+  return {}
 }
 
-export const appContext = global.appContext
+const globalNamespace = chooseGlobalNamespace()
+
+if (typeof globalNamespace.appContext === 'undefined') {
+  globalNamespace.appContext = {}
+}
+
+export const appContext = globalNamespace.appContext
 
 // Use a function to give us more possibilities in future
 export default () => appContext
